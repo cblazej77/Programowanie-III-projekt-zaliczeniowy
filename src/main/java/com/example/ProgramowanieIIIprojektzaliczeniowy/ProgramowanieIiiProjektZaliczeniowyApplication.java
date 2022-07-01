9fc8bdcca1d6d07637d20ca5d64abb7061aa3992
RadzkiK <k.domeradzki@outlook.com>
2022-07-01 20:03:07 +0200

    Zamiana na wersję Spring

---
+++ src/main/java/com/example/ProgramowanieIIIprojektzaliczeniowy/ProgramowanieIiiProjektZaliczeniowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgramowanieIIIprojektzaliczeniowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramowanieIiiProjektZaliczeniowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramowanieIiiProjektZaliczeniowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgramowanieIIIprojektzaliczeniowy/ProgramowanieIiiProjektZaliczeniowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgramowanieIIIprojektzaliczeniowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramowanieIiiProjektZaliczeniowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
