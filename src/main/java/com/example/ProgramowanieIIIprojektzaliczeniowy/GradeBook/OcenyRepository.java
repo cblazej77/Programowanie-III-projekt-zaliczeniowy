71ac7d56e8f5511599bfb428f0bbb95826e43327
Błażej Cieślewicz <102741487+cblazej77@users.noreply.github.com>
2022-07-21 22:05:26 +0200

    Delete src directory

--- src/main/java/com/example/ProgramowanieIIIprojektzaliczeniowy/GradeBook/OcenyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.ProgramowanieIIIprojektzaliczeniowy.GradeBook;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OcenyRepository extends JpaRepository<Oceny, Long> {
-}
