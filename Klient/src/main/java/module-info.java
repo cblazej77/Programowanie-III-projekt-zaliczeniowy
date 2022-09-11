6c495ecfd7b94d7d088389bcee9ca4d0f90ef14a
Błażej Cieślewicz <102741487+cblazej77@users.noreply.github.com>
2022-09-12 00:10:33 +0200

    deleting entities update

--- Klient/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.zaliczenieklient2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires android.json;
-
-
-    opens com.example.zaliczenieklient2 to javafx.fxml;
-    exports com.example.zaliczenieklient2;
-}
