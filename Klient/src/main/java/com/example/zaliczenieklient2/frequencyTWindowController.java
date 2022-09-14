b025bc85f65d3e5024518267b6a28a1231d091dc
Błażej Cieślewicz <102741487+cblazej77@users.noreply.github.com>
2022-09-15 01:31:41 +0200

    frequencyT edit

---
+++ Klient/src/main/java/com/example/zaliczenieklient2/frequencyTWindowController.java
@@ -0,0 +1,13 @@
+package com.example.zaliczenieklient2;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class frequencyTWindowController implements Initializable {
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
