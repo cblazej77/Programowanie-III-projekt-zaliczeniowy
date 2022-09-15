c86fd83eb0bc14494006adaa7d6a7ce2f8a8b3a3
RadzkiK <104510164+RadzkiK@users.noreply.github.com>
2022-09-15 20:11:32 +0200

    testy

--- Serwer/src/main/java/Main.java
+++ Serwer/src/main/java/Main.java
@@ -11,21 +11,57 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*Querries querries = new Querries();
+        Querries querries = new Querries();
 
-        for(FrekwencjaEntity f: querries.findFrekwencjaByNrWDzienniku(7, "A2021")) {
-            System.out.println(f);
+        /* dodanie lekcji 1 rok */ /*
+        LocalDate date = LocalDate.parse("2022-09-05", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        for(int i = 0; i < 1; i++) {
+        querries.addLekcjaByLoginAndNames(1,"2A2021", "KD017", "JezykPolski", Date.valueOf(date), "");
+        querries.addLekcjaByLoginAndNames(2,"2A2021", "KD018", "Informatyka", Date.valueOf(date), "");
+        querries.addLekcjaByLoginAndNames(3,"2A2021", "KD016", "Matematyka", Date.valueOf(date), "");
+        querries.addLekcjaByLoginAndNames(1,"1B2022", "KD018", "Informatyka", Date.valueOf(date), "");
+        querries.addLekcjaByLoginAndNames(2,"1B2022", "KD016", "JezykAngielski", Date.valueOf(date), "");
+        querries.addLekcjaByLoginAndNames(3,"1B2022", "KD017", "JezykPolski", Date.valueOf(date), "");
+        querries.addLekcjaByLoginAndNames(1,"2A2021", "KD016", "JezykAngielski", Date.valueOf(date.plusDays(1)), "");
+        querries.addLekcjaByLoginAndNames(2,"2A2021", "KD017", "JezykPolski", Date.valueOf(date.plusDays(1)), "");
+        querries.addLekcjaByLoginAndNames(3,"2A2021", "KD016", "Matematyka", Date.valueOf(date.plusDays(1)), "");
+        querries.addLekcjaByLoginAndNames(1,"1B2022", "KD017", "JezykPolski", Date.valueOf(date.plusDays(1)), "");
+        querries.addLekcjaByLoginAndNames(2,"1B2022", "KD016", "Matematyka", Date.valueOf(date.plusDays(1)), "");
+        querries.addLekcjaByLoginAndNames(3,"1B2022", "KD016", "JezykAngielski", Date.valueOf(date.plusDays(1)), "");
+        querries.addLekcjaByLoginAndNames(1,"2A2021", "KD016", "Matematyka", Date.valueOf(date.plusDays(2)), "");
+        querries.addLekcjaByLoginAndNames(2,"2A2021", "KD018", "Informatyka", Date.valueOf(date.plusDays(2)), "");
+        querries.addLekcjaByLoginAndNames(3,"2A2021", "KD017", "JezykPolski" , Date.valueOf(date.plusDays(2)), "");
+        querries.addLekcjaByLoginAndNames(1,"1B2022", "KD017", "JezykPolski", Date.valueOf(date.plusDays(2)), "");
+        querries.addLekcjaByLoginAndNames(2,"1B2022", "KD016", "JezykAngielski", Date.valueOf(date.plusDays(2)), "");
+        querries.addLekcjaByLoginAndNames(3,"1B2022", "KD016", "Matematyka", Date.valueOf(date.plusDays(2)), "");
+        querries.addLekcjaByLoginAndNames(1,"2A2021", "KD016", "JezykAngielski", Date.valueOf(date.plusDays(3)), "");
+        querries.addLekcjaByLoginAndNames(2,"2A2021", "KD017", "JezykPolski", Date.valueOf(date.plusDays(3)), "");
+        querries.addLekcjaByLoginAndNames(3,"2A2021", "KD016", "Matematyka", Date.valueOf(date.plusDays(3)), "");
+        querries.addLekcjaByLoginAndNames(1,"1B2022", "KD017", "JezykPolski", Date.valueOf(date.plusDays(3)), "");
+        querries.addLekcjaByLoginAndNames(2,"1B2022", "KD016", "Matematyka", Date.valueOf(date.plusDays(3)), "");
+        querries.addLekcjaByLoginAndNames(3,"1B2022", "KD016", "JezykAngielski", Date.valueOf(date.plusDays(3)), "");
+        querries.addLekcjaByLoginAndNames(1,"2A2021", "KD017", "JezykPolski", Date.valueOf(date.plusDays(4)), "");
+        querries.addLekcjaByLoginAndNames(2,"2A2021", "KD018", "Informatyka", Date.valueOf(date.plusDays(4)), "");
+        querries.addLekcjaByLoginAndNames(3,"2A2021", "KD016", "Matematyka", Date.valueOf(date.plusDays(4)), "");
+        querries.addLekcjaByLoginAndNames(1,"1B2022", "KD018", "Informatyka", Date.valueOf(date.plusDays(4)), "");
+        querries.addLekcjaByLoginAndNames(2,"1B2022", "KD016", "JezykAngielski", Date.valueOf(date.plusDays(4)), "");
+        querries.addLekcjaByLoginAndNames(3,"1B2022", "KD017", "JezykPolski", Date.valueOf(date.plusDays(4)), "");
+        date = date.plusDays(7);
+        } */
+        /*for(String e: querries.findLekcjePrzedmiotForPrzedmiotByUserLogin("KD001", Date.valueOf("2022-09-12"))) {
+        System.out.println(e);
+        } */
+
+
+        //System.out.println(querries.czyNauczycielUczyKlase("KD016", "1B2022"));
+        //System.out.println(querries.czyNauczycielUczyKlasePrzedmiotu("KD016", "Matematyka","1B2022"));
+
+        for(String s: querries.findImionaUczniowZKlasy("1B2022")) {
+            System.out.println(s);
         }
 
-        System.out.println();
+        System.out.println(querries.findKlaseWychowawcy("KD016"));
 
-        for(FrekwencjaEntity f: querries.findFrekwencjaByImieINazwisko("Karolina", "Sumosia")) {
-            System.out.println(f);
-        }
-
-        System.out.println();
-
-        System.out.println(querries.findAvgOfOcenyforUczenFromPrzedmiot("matematyka",0L));*/
         System.out.println("Czekam...");
         try{
             ServerSocket server = new ServerSocket(8081);
